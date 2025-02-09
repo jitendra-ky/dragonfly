b59a5c68a8163dd4f07a0edf311fc8d639f868e3
jitendra-ky <jk69854@gmail.com>
2025-02-09 15:19:47 +0530

    refactor urls: change the zserver urls from "api/" to "" (root).
    this commit change the zserver urls from "api/" to "" (root)
    so now zserver would be the one and only app of my django project.
    which sever all the templates and api endpoints.

--- zserver/urls.py
+++ zserver/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("user-profile/", views.UserProfileView.as_view(), name="user-profile"),
-    path("sign-in/", views.SignInView.as_view(), name="sign-in"),
-    path("sign-up-otp/", views.SignUpOTPView.as_view(), name="sign-up-otp"),
+    path("api/user-profile/", views.UserProfileView.as_view(), name="user-profile"),
+    path("api/sign-in/", views.SignInView.as_view(), name="sign-in"),
+    path("api/sign-up-otp/", views.SignUpOTPView.as_view(), name="sign-up-otp"),
 ]
