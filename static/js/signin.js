b9b8a12953585cc9057bf9221a8bc8f747dd66e6
jitendra-ky <jk69854@gmail.com>
2025-02-19 01:53:57 +0530

    UI Google-login: Implement Google Auth in signin and signup flows.

--- static/js/signin.js
+++ static/js/signin.js
@@ -9,6 +9,68 @@ $(function () {
         return;
     }
 
+    function redirectToGoogleOAuth() {
+        // Your OAuth 2.0 Client ID
+        const clientId = env_var.GOOGLE_CLIENT_ID;
+        
+        // The redirect URI
+        const redirectUri = env_var.GOOGLE_REDIRECT_URI; // Replace with your actual redirect URI
+
+        // The scopes you're requesting access to
+        const scope = 'email profile openid';
+
+        // Build the URL for Google's OAuth 2.0 authorization endpoint
+        const authorizationUrl = `https://accounts.google.com/o/oauth2/v2/auth?` +
+            `scope=${encodeURIComponent(scope)}&` +
+            `response_type=code&` +
+            `client_id=${clientId}&` +
+            `redirect_uri=${encodeURIComponent(redirectUri)}&` +
+            `state=state_parameter_passthrough_value`;
+
+        // Redirect the user to the Google authorization page
+        window.location.href = authorizationUrl;
+    }
+
+    function loginWithAuthCode(authorizationCode) {
+        $.ajax({
+            url: '/google-login/',
+            method: 'POST',
+            data: JSON.stringify({ code: authorizationCode }),
+            contentType: 'application/json',
+            success: function (response) {
+                console.log(response);
+                if (response.session_id) {
+                    console.log('Login successful!');
+                    // set session id to cookies
+                    document.cookie = `session_id=${response.session_id}; path=/`;
+                    window.location.href = '/'; // Redirect after login
+                } else {
+                    alert('Login failed!');
+                }
+            },
+            error: function (response) {
+                console.log(response);
+            }
+        });
+    }
+
+
+    // Trigger Google login when the button is clicked
+    $('#google-login-btn').on('click', function () {
+        console.log('Google login clicked');
+        console.log(window.location.origin);
+        redirectToGoogleOAuth();
+    });
+
+    // Check if the URL contains an authorization code
+    // If it does, log the user in
+    const urlParams = new URLSearchParams(window.location.search);
+    const authorizationCode = urlParams.get('code');
+    if (authorizationCode) {
+        loginWithAuthCode(authorizationCode);
+        console.log(authorizationCode);
+    }
+
     $('#signin-form').on('submit', function (event) {
         event.preventDefault();
         const formData = {
