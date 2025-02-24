3900f3c0c1b11aaaf048fbaf0df181da035fe126
jitendra-ky <jk69854@gmail.com>
2025-02-24 17:53:59 +0530

    test UI: log setup time for UI tests.

--- tools/tests/ui_all.py
+++ tools/tests/ui_all.py
@@ -1,8 +1,13 @@
+import time
+
 from common import setup_module, teardown_module
 from ui_signin import test_signin, test_signin_with_google
 from ui_signup import test_signup
 
+t1 = time.time()
 driver = setup_module()
+t2 = time.time()
+print(f"Setup time: {t2 - t1:.2f}s")
 
 test_signin(driver)
 test_signup(driver)
